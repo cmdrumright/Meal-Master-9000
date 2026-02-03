5220d462183497308db8715416a0bf4648657c57
cmdrumright <cdrumright@protonmail.com>
2026-02-03 10:26:49 -0600

    Added material UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import { Login } from "./components/auth/Login";
 import { Register } from "./components/auth/Register";
 import { Authorized } from "./views/Authorized";
 import { ApplicationViews } from "./views/ApplicationViews";
+import { useColorScheme } from "@mui/material";
 
 export const App = () => {
+  const { mode, setMode } = useColorScheme();
+  if (!mode) {
+    return null;
+  }
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { theme } from "./theme.js";
+import { ThemeProvider } from "@mui/material";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
 );
